067baf17402ebdf54aef5c296fa27690f67c7ef3
jorgejimenez847 <jorgejimn847@gmail.com>
2025-08-28 20:55:58 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://jorgejimenez847.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
